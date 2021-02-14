eae972e5e8c102f0e7aea695b9f8198a5f4b54b9
erdal dalkiran <erdal.dalkiran@trendyol.com>
2021-02-14 14:08:22 +0300

    initial commit

---
+++ src/main/java/com/erdaldalkiran/cdcdemo/CdcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.erdaldalkiran.cdcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erdaldalkiran/cdcdemo/CdcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erdaldalkiran.cdcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
